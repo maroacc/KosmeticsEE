6638af8aef303014acbd8e0eec03db0eddbbad33
maroacc <45521548+maroacc@users.noreply.github.com>
2020-03-31 16:11:51 +0200

    Conector y DAO

--- src/AppAddBrand.java
+++ src/AppAddBrand.java
@@ -7,19 +7,8 @@ import java.util.Scanner;
 
 public class AppAddBrand {
     public static void main(String[] args) {
-
-        //Carga la clase
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
             System.out.println("El programa se está ejecutando...");
             System.out.println(DAOBrands.addBrand(new Brand("name", "username", "password", "userEmail", "webAddress", "contactEmail", "description", "fiscalAddress", "CIF", "tipoPago", true, "companyName", "contactPerson", "contactDirection")));
-
-
-
-        } catch (ClassNotFoundException e) {
-            //Salta si no se ha encontrado la clase
-            e.printStackTrace();
-        }
 
     }
 }

2cad5339f1e37f2dcf721020c2de5cfd3802bbe5
nicolasarm25@gmail.com <31Dic1998>
2020-04-18 14:15:35 +0200

    Productos

--- src/Servlets/ServletProducto.java
+++ src/Servlets/ServletProducto.java
@@ -16,7 +16,7 @@ public class ServletProducto extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        new Product(request.getParameter("inputNombre"), request.getParameter("inputDescripcion"), request.getParameter("exampleCategoria"), Float.parseFloat(request.getParameter("inputPrecio")), Integer.parseInt(request.getParameter("inputDescuento")));
+        new Product(request.getParameter("inputNombre"), request.getParameter("validationTextarea"), request.getParameter("exampleCategoria"), Float.parseFloat(request.getParameter("inputPrecio")), Integer.parseInt(request.getParameter("inputDescuento")));
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

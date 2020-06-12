b7cc5e9c8e2acdfcca3f66c6b4bfbf68c8f3399f
maroacc <45521548+maroacc@users.noreply.github.com>
2020-06-12 16:13:44 +0200

    Plantilla en tablero.html para ofertas.jsp. Comenzados los toggles en ofertas.js.

--- web/js/ofertas.js
+++ web/js/ofertas.js
@@ -5,3 +5,43 @@ $(document).ready(function(){
         $("#radio-fija").hide();
     });
 });*/
+
+$(document).ready(function(){
+    $("#input-fija").hide();
+    $("#card-producto").hide();
+    $("#search-productos-especificos").hide();
+
+    $("#radio-porcentaje").click(function(){
+        $("#card-valor").show();
+        $("#input-porcentaje").show();
+        $("#input-fija").hide();
+        $("#card-producto").hide();
+
+    });
+
+    $("#radio-fija").click(function(){
+        $("#card-valor").show();
+        $("#input-porcentaje").hide();
+        $("#input-fija").show();
+        $("#card-producto").hide();
+
+    });
+
+    $("#radio-envio").click(function(){
+        $("#card-valor").hide();
+        $("#card-producto").hide();
+    });
+
+    $("#radio-x-y").click(function(){
+        $("#card-valor").hide();
+        $("#card-producto").show();
+    });
+
+    $("#input-pedido-completo").click(function(){
+        $("#search-productos-especificos").hide();
+    });
+
+    $("#input-productos-especificos").click(function(){
+        $("#search-productos-especificos").show();
+    });
+});
